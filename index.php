41416df2296ed971a1f84cf7d9d6edc0f07c1cb8
SEELFMADE <Fourchizzyadilson@gmail.com>
2021-11-30 16:57:15 +0100

    adicao do heder e load das dependências

--- index.php
+++ index.php
@@ -1,2 +1,14 @@
 <?php
-
+require_once 'files/crud.php';
+?>
+<!DOCTYPE html>
+<html lang="pt-AO">
+<head>
+<meta charset="UTF-8">
+    <link rel="stylesheet" href="dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="dist/css/nosso.css">
+    <title>Projecto</title>
+</head>
+<body>   
+</body>
+</html>
