77bcd74a4af0171f0c9598ecef384c13a6b95499
SEELFMADE <Fourchizzyadilson@gmail.com>
2021-11-30 16:59:15 +0100

    Adicionada cadastro e alteração de alunos

--- index.php
+++ index.php
@@ -28,5 +28,51 @@ require_once 'files/crud.php';
             </div>
         </div>
     </section>
+
+    
+    <!-- CADASTRO -->
+    <section class="py-5 bg-light" id="cadastro">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-6 height">
+                <div class="card card-body bg-secondary">
+                    <div class="card-title text-center text-white">
+                        <h2 class="text-capitalize">Estu
+                            dantes</h2>
+                        <p>Cadastro</p>
+                    </div>
+                    <form action="files/crud.php" method="POST">
+                            <input type="hidden" name="id" value="<?php echo $id ?>">
+                        <div class="form-group  mt-3">
+                            <input type="nome" name="nome" class="form-control" value="<?php echo $nome ?>" placeholder="Nome">
+                        </div>
+                        <div class="form-group  mt-3">
+                            <input type="bi" name="bi" value="<?php echo $bi ?>" class="form-control" placeholder="BI">
+                        </div>
+                        <div class="form-group  mt-3">
+                            <select name="turma" class="form-control">
+                                <option value="A1">A1</option>
+                                <option value="A2">A2</option>
+                                <option value="A3">A3</option>
+                                <option value="A4">A4</option>
+                            </select>
+                        </div>
+                        <div class="form-group  mt-3">
+                            <?php if($actualiza==true){
+                            ?>                           
+                            <input type="submit" name="salvar" class="btn btn-primary" value="Cadastrar">
+                            <?php }else{
+                                ?>
+                                 <input type="submit" name="actualizar" class="btn btn-primary" value="actualizar">
+                           <?php } ?>
+                        </div>                     
+                    </form>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 height" style="background-image: url(./dist/img/fundoHeader.jpg);">
+            </div>
+        </div>
+        </div>
+        </section>
 </body>
 </html>
